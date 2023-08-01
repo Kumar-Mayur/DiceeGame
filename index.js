1d5600fdeae768155941d4a9702e2bdd837a782b
Kumar Mayur <60908650+mayurkaran21@users.noreply.github.com>
2023-08-01 18:32:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSrc = randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSrc);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSrc2 = "dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSrc2);
+
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = " Play 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! ";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
